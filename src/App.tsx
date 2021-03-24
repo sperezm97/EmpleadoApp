c16cbdfe82bff5091edd7e5caa38bea728cba1e9
Sebastian Perez <sperezm97@gmail.com>
2021-03-24 10:39:55 -0400

    Refactor App Navigator component

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,15 @@
 import { ThemeProvider } from 'hooks/useTheme';
+import { AppNavigator } from 'navigator/AppNavigator';
 import React from 'react';
-import { ProfileScreen } from 'screens/profile/ProfileScreen';
-import { SearchScreen } from 'screens/search/SearchScreen';
-import { SettingsScreen } from 'screens/settings/SettingsScreen';
-
-import { BottomTabNavigationProp, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-
-export type EmParamsProps = {
-  Settings: undefined;
-  Search: undefined;
-  Profile: undefined;
-};
-
-export type EmTabNavigationProp = BottomTabNavigationProp<EmParamsProps>;
-
-const Tab = createBottomTabNavigator<EmParamsProps>();
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Colors } from 'theme';
 
 export const App = () => {
   return (
-    <ThemeProvider>
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name="Settings" component={SettingsScreen} />
-          <Tab.Screen name="Search" component={SearchScreen} />
-          <Tab.Screen name="Profile" component={ProfileScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </ThemeProvider>
+    <SafeAreaView style={{ flex: 1, backgroundColor: Colors.background }}>
+      <ThemeProvider>
+        <AppNavigator />
+      </ThemeProvider>
+    </SafeAreaView>
   );
 };
