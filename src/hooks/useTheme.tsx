341aea628aa36b9e5302b13e9f9dab0591e80865
Sebastian Perez <sperezm97@gmail.com>
2021-03-24 10:19:34 -0400

    Fix Theme Provider

--- src/hooks/useTheme.tsx
+++ src/hooks/useTheme.tsx
@@ -4,7 +4,9 @@ import Theme, { Theme as TypeTheme } from 'theme';
 const ThemeContext = React.createContext<TypeTheme>(Theme);
 
 export const ThemeProvider: React.FC = ({ children }) => {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return (
+    <ThemeContext.Provider value={Theme}>{children}</ThemeContext.Provider>
+  );
 };
 
 export const useTheme = () => {
