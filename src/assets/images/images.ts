f85dabfbf28b1d13004f09c63fc23fa54210a5b4
Sebastian Perez <sperezm97@gmail.com>
2021-03-24 10:20:11 -0400

    Add Missing image for Category Cards

---
+++ src/assets/images/images.ts
@@ -0,0 +1,12 @@
+export const images = {
+  dba: require('./baseDeDatos.png'),
+  webDev: require('./web.png'),
+  mobileDev: require('./movil.png'),
+  network: require('./redes.png'),
+  sysAdmin: require('./sistemas.png'),
+  graphicDesigner: require('./disenoGrafico.png'),
+  softwareDev: require('./software.png'),
+  itSales: require('ventas.png'),
+  logo: require('./splashLogo.png'),
+  gameDev: require('./videoJuegos.png'),
+};
